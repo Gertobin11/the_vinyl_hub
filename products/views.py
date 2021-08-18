ca3cd92433f5147ddce6b428fbe634dbf09035fa
Gertobin11 <gertobin11@gmail.com>
2021-08-18 00:38:00 +0000

    add product details views , template and url and styled it

--- products/views.py
+++ products/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
-
-products = Product.objects.all()
-
-context = {
-    'products': products
-}
 
 
 def all_products(request):
     ''' A view to show all the products including  searches and sorting'''
+
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    ''' A view to show the user their selected product'''
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product
+    }
+
+    return render(request, 'products/product_details.html', context)
