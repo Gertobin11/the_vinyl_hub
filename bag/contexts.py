34ce2de38b6a69033a32a95f928909c3f1a84f3c
Gertobin11 <gertobin11@gmail.com>
2021-08-19 23:26:46 +0000

    added code to check if the items in the bag are 2 from the chosen sale category and if they are provide a 20% discount to those items and checked the funcioanlity of the code

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,37 +1,74 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 from decimal import Decimal
 
 
 def bag_contents(request):
     bag_items = []
+    discount = 0
     sale_items = []
+    subtotal = 0
+    subtotals = []
+    new_total = 0
     total = 0
     product_count = 0
+    bag = request.session.get('bag', {})
 
-    # function to check if more then 1 hip hop item were bought
-    for bag_item in bag_items:
-        if bag_item.product.category == 'hip_hop':
-            sale_items.append(bag_item)
-            if sale_items > 1:
-                for sale_item in sale_items:
-                    sale_item.product.price = (sale_item.product.price
-                                               * Decimal(20 / 100))
-                    bag_items.append(sale_item)
-            else:
-                for sale_item in sale_items:
-                    bag_items.append(sale_item)
+    for item_id, item_data in bag.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.price
+        product_count += item_data
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': item_data,
+            'product': product,
+        })
+
+    ''' code to check if more then 1 hip hop item were bought
+        it first check if the item has the chosen category which is on sale.
+        then checks if there are more then 2. If the conditions are met it
+        applies a 20% discount '''
+    if bag_items:
+        for bag_item in bag_items:
+            if bag_item['product'].category.name == 'hip_hop':
+                sale_items.append(bag_item)
+                bag_items.remove(bag_item)
+        if len(sale_items) > 1 or sale_items[0].get('quantity') > 1:
+            for sale_item in sale_items:
+                sale_item['product'].price = (sale_item['product'].price -
+                                              (sale_item['product'].price
+                                               * 20 / 100))
+                sale_item['product'].price = Decimal(sale_item['product']
+                                                     .price).quantize(
+                                                     Decimal('.01'))
+                bag_items.append(sale_item)
+        else:
+            for sale_item in sale_items:
+                bag_items.append(sale_item)
+
+    if bag_items:
+        for bag_item in bag_items:
+            subtotal = Decimal(bag_item['quantity']) * bag_item['product'].price
+            subtotals.append(subtotal)
+
+    new_total = sum(subtotals)
+            
+    discount = total - new_total
+
+
 
     ''' Code to see if free delivery criteria has been met,
      learned during Code Institute course '''
     if total < settings.FREE_DELIVERY_THRESHOLD:
-        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        delivery = new_total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = delivery + total
+    grand_total = delivery + new_total
 
     context = {
         'bag_items': bag_items,
@@ -41,6 +78,13 @@ def bag_contents(request):
         'free_delivery_delta': free_delivery_delta,
         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
         'grand_total': grand_total,
+        'discount': discount
     }
+
+    print(new_total)
+    print(discount)
+    print(grand_total)
+    print(total)
+    print(delivery)
 
     return context
