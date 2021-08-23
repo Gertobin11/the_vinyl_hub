6f25cebc46813813bdcb1095f5c327638231b611
Gertobin11 <gertobin11@gmail.com>
2021-08-23 22:41:09 +0000

    added fail safe tat webhooks will complete the order form with bag contents and pid

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -4,6 +4,7 @@ from .models import Order, OrderLineItem
 from products.models import Product
 
 import json
+import time
 
 
 class StripeWH_Handler:
@@ -37,15 +38,10 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
-        
+
         order_exists = False
-        if order_exists:
-            return HttpResponse(
-                content=f'Webhook received: {event["type"]}'
-                f'| SUCCESS: Verified order already in database',
-                status=200)
-        else:
-            order = None
+        attempt = 1
+        while attempt <= 5:
             try:
                 order = Order.objects.get(
                     full_name__iexact=shipping_details.name,
@@ -60,7 +56,34 @@ class StripeWH_Handler:
                     grand_total=grand_total,
                     original_bag=bag,
                     stripe_pid=pid,
-                    )
+                )
+                order_exists = True
+                break
+            except Order.DoesNotExist:
+                attempt += 1
+                time.sleep(1)
+
+        if order_exists:
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]}'
+                f'| SUCCESS: Verified order already in database',
+                status=200)
+        else:
+            order = None
+            try:
+                order = Order.objects.create(
+                    full_name=shipping_details.name,
+                    email=billing_details.email,
+                    phone_number=shipping_details.phone,
+                    country=shipping_details.address.country,
+                    postcode=shipping_details.address.postal_code,
+                    town_or_city=shipping_details.address.city,
+                    street_address1=shipping_details.address.line1,
+                    street_address2=shipping_details.address.line2,
+                    county=shipping_details.address.state,
+                    original_bag=bag,
+                    stripe_pid=pid,
+                )
                 for item_id, item_data in json.loads(bag).items():
                     product = Product.objects.get(id=item_id)
                     order_line_item = OrderLineItem(
