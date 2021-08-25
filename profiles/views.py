77755eec704f1dbc6e598994b6885a5fa3ae8587
Gertobin11 <gertobin11@gmail.com>
2021-08-25 10:22:35 +0000

    added profile functionality to prefill the order form and added the users previous orders to the profile page

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 
 def profile(request):
@@ -31,6 +32,24 @@ def profile(request):
         'orders': orders,
         'profile': profile,
         'on_profile': True
+    }
+
+    return render(request, template, context)
+
+
+def order_history(request, order_number):
+    ''' A view to render the users past orders '''
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, (
+        f'This is a past confirmation for order number {order_number}.'
+        'A confirmation email was sent on the order date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True
     }
 
     return render(request, template, context)
