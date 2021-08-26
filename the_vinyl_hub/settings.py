1a3bcb0d9104996d472c6d2efb596d9927f1ce82
Gertobin11 <gertobin11@gmail.com>
2021-08-26 12:45:57 +0000

    edited allowed hosts

--- the_vinyl_hub/settings.py
+++ the_vinyl_hub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['the_vinyl_hub.heroku.app', 'localhost']
+ALLOWED_HOSTS = ['the_vinyl_hub.herokuapp.com', 'localhost']
 
 
 # Application definition
