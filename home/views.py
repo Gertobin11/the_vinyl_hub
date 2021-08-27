8f71931050a6fd8b680e8d20413e44a96a329a15
Gertobin11 <gertobin11@gmail.com>
2021-08-27 10:40:57 +0000

    added last touch ups

--- home/views.py
+++ home/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def index(request):
     ''' A view to return the index page '''
     return render(request, 'home/index.html')
+
+
+def requests(request):
+    ''' A view to return requests '''
+    return render(request, 'home/requests.html')
